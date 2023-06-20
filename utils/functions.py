5d8f40f5c4ab21fcb9982c6f290ee190a2c46611
Zhanna Popova <zh.denisovna.97@gmail.com>
2023-06-21 00:26:54 +0500

    write functions for formating date, account number of sender and recipient

--- utils/functions.py
+++ utils/functions.py
@@ -1,4 +1,7 @@
 import json
+import re
+from datetime import datetime
+
 
 def get_all_operation():
     """
@@ -33,3 +36,49 @@ def get_last_five_executed_operations():
     last_five_executed_operations = sorted_executed_operations[:5]
 
     return last_five_executed_operations
+
+
+def get_formatted_date(date):
+    """
+    функция для форматирования полученной даты в формат ДД.ММ.ГГГГ
+    """
+    datetime_object = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%f")
+    formatted_date = datetime_object.strftime("%d.%m.%Y")
+    return formatted_date
+
+
+def get_masked_number(who_send):
+    """
+    функция, позволяющая замаскировать номер карты в формате  XXXX XX** **** XXXX
+    """
+    if len(who_send) > 0:
+        # разделение строки с инфой о карте на буквенную и численную часть, где буквы - там пробелы после каждой
+        # большой буквы, для корректности вывода карт, состоящих из более чем 1 слова
+        card_name = ''.join(filter(str.isalpha, who_send))
+        card_name_with_chunks = re.sub(r'(?=[A-Z])', " ", card_name)
+        card_name_with_chunks_ = card_name_with_chunks.replace(" ", "", 1)
+
+        # работа с численной составляющшей, маскировка *
+        card_account = ''.join(filter(str.isdigit, who_send))
+        masked_card_account = card_account[:6]
+        masked_card_account += "*" * (len(card_account) - 6 - 4)
+        masked_card_account += who_send[-4:]
+
+        # разделение на блоки по 4 и вывод полного наименования карты и номера счета
+        chunks = " ".join([masked_card_account[i:i + 4] for i in range(0, len(masked_card_account), 4)])
+        result = f"{card_name_with_chunks_} {chunks}"
+
+        return result
+    # возвращается, если отсутствует информация, откуда перевод
+    return "Неизвестный счет"
+
+
+def get_masked_account(who_get):
+    """
+    функция для маскировки номе6ра счета получателя в формате **XXXX
+    """
+    account_name = ''.join(filter(str.isalpha, who_get))
+    account_number = ''.join(filter(str.isdigit, who_get))
+    masked_account = "**" + account_number[-4:]
+    result = f"{account_name} {masked_account}"
+    return result
